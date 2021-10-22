4caf3d61a971531f05ca6d716d1c28ea740bbc99
michael-fourie <17mlf1@queensu.ca>
2021-10-22 12:34:13 -0400

    functional component no web kit text

--- client/src/components/WelcomeText.js
+++ client/src/components/WelcomeText.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
 import '../style/WelcomeText.css'
-import logo from '../img/144.JPG'
-class WelcomeText extends Component{
+
+
+function WelcomeText() {
     
-    render(){
         return(
             <div>
                 <h1 className='title'>144NFT</h1>
                 <p className="desc">A Collaborative Pixel Art Masterpiece on Ethereum</p>
             </div>
         )
-    }
+    
 }
 
 export default WelcomeText
